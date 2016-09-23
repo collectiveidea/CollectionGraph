8000d892c2f9d38da47ec002a8abf92ad34a2e41
be-bert <bebert.apps@gmail.com>
2016-09-23 13:46:40 -0400

    WIP

---
+++ CollectionGraph/CollectionGraphView.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionGraphView.swift
+//  CollectionGraph
+//
+//  Created by Ben Lambert on 9/23/16.
+//  Copyright © 2016 Collective Idea. All rights reserved.
+//
+
+import UIKit
+
+class CollectionGraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
