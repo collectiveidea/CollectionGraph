65c936d0c7ccc0b6690f02f5e68929a5c5ecd226
be-bert <bebert.apps@gmail.com>
2016-09-27 16:15:04 -0400

    Added a GraphType. Added Required init with graph type. Added layout var.

--- CollectionGraph/CollectionGraphView.swift
+++ CollectionGraph/CollectionGraphView.swift
@@ -11,9 +11,61 @@ import UIKit
 @IBDesignable
 public class CollectionGraphView: UIView {
 
+    public enum GraphType {
+        case Dot, Line, Bar
+    }
+
+    var type: GraphType? {
+        didSet {
+            if let type = type {
+                switch type {
+                case .Dot:
+                    // TODO: - update with Dot layout subclass
+                    layout = UICollectionViewLayout()
+                case .Line:
+                    // TODO: - update with Line layout subclass
+                    layout = UICollectionViewLayout()
+                case .Bar:
+                    // TODO: - update with Bar layout subclass
+                    layout = UICollectionViewLayout()
+                }
+            }
+        }
+    }
+
+    @IBOutlet public var layout: UICollectionViewLayout? {
+        didSet {
+            print("* Set Layout *")
+        }
+    }
+
     @IBInspectable public var myColor: UIColor = UIColor.red {
         didSet {
             backgroundColor = myColor
         }
     }
+
+    // MARK: - View Lifecycle
+
+    required public init(frame: CGRect, type: GraphType) {
+        super.init(frame: frame)
+
+        self.type = type
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
 }
+
+
+
+// CollectionGraphView
+// |
+// |+ UICollectionView
+// |+ UICollectionViewDelegate
