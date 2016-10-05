3482037138f29b7caa282e54e1998691299115c6
be-bert <bebert.apps@gmail.com>
2016-10-05 16:33:00 -0400

    Added ReuseID enum for storing Cell IDs.

--- CollectionGraph/CollectionGraphView.swift
+++ CollectionGraph/CollectionGraphView.swift
@@ -21,6 +21,12 @@ public enum ReuseIDs: String {
     case XLabelView = "XLabel"
 }
 
+public enum ReuseIDs: String {
+    case GraphCell = "GraphCell"
+    case LineSupplementaryView = "LineView"
+    case BarSupplementaryView = "BarView"
+}
+
 @IBDesignable
 public class CollectionGraphView: UIView {
 
