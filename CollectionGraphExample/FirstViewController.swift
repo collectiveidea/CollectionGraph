0d3fc64eecc948e5c3b89d4ee462112f10bbc7b2
be-bert <bebert.apps@gmail.com>
2016-10-06 10:02:25 -0400

    Removed white space.

--- CollectionGraphExample/FirstViewController.swift
+++ CollectionGraphExample/FirstViewController.swift
@@ -16,8 +16,6 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-
     }
 
     override func didReceiveMemoryWarning() {
