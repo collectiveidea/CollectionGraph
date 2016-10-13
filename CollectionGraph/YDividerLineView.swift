6c63f2d71f18b17604a1f5506e771aeae9b28591
be-bert <bebert.apps@gmail.com>
2016-10-13 15:50:25 -0400

    Add an inset to the yDividerLines based on the Collection View's left inset.

--- CollectionGraph/YDividerLineView.swift
+++ CollectionGraph/YDividerLineView.swift
@@ -14,7 +14,7 @@ class YDividerLineView: UICollectionReusableView {
     var line: CAShapeLayer = CAShapeLayer()
     var lineWidth: CGFloat = 1
     var lineColor: UIColor = UIColor (red: 0.2353, green: 0.2353, blue: 0.2353, alpha: 1.0)
-    var gutter: CGFloat = 0
+    var inset: CGFloat = 0
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -42,21 +42,21 @@ class YDividerLineView: UICollectionReusableView {
     override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
 
         if let attributes = layoutAttributes as? YDividerLayoutAttributes {
-            lineColor = UIColor.lightGray// attributes.lineColor
+            lineColor = attributes.lineColor
 
-//            gutter = attributes.gutter
+            inset = attributes.inset
 
-            label.textColor = UIColor.lightGray// attributes.textColor
+            label.textColor = attributes.textColor
             label.text = attributes.text
 
             label.sizeToFit()
 
-            label.center = CGPoint(x: gutter / 2, y: 0)
+            label.center = CGPoint(x: inset / 2, y: 0)
         }
     }
 
     func drawLine() {
-        let start = CGPoint(x: self.bounds.minX + gutter, y: self.bounds.minY)
+        let start = CGPoint(x: self.bounds.minX + inset, y: self.bounds.minY)
         let end = CGPoint(x: self.bounds.maxX, y: self.bounds.minY)
 
         let path = UIBezierPath()
