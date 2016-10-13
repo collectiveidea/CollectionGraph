3234db03f231879448563515a760202d45872c77
be-bert <bebert.apps@gmail.com>
2016-10-13 17:06:41 -0400

    Deque the line connector view for use in the collection view.

--- CollectionGraph/CollectionGraphDataSource.swift
+++ CollectionGraph/CollectionGraphDataSource.swift
@@ -35,6 +35,10 @@ class CollectionGraphDataSource: NSObject, UICollectionViewDataSource {
         case ReuseIDs.YDividerSupplementaryView.rawValue:
 
             return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: ReuseIDs.YDividerSupplementaryView.rawValue, for: indexPath)
+            
+        case ReuseIDs.LineSupplementaryView.rawValue:
+            
+            return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: ReuseIDs.LineSupplementaryView.rawValue, for: indexPath)
 
         default:
 
