fa16b20d8b68cccc6a0a1f0a3222152e71f51255
be-bert <bebert.apps@gmail.com>
2016-10-13 16:37:38 -0400

    Adjust the content offset of the collectionView.

--- CollectionGraphExample/FirstViewController.swift
+++ CollectionGraphExample/FirstViewController.swift
@@ -21,11 +21,10 @@ class FirstViewController: UIViewController {
         graph.layout?.graphWidth = 400
 
         let cell = UICollectionViewCell(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
-        cell.contentView.backgroundColor = UIColor.red
 
         graph.graphCell = cell
 
-        graph.graphData = GraphData(data: [[CGPoint(x: 1, y: 1), CGPoint(x: 2, y: 2), CGPoint(x: 4, y: 5)]])
+        graph.graphData = GraphData(data: [[CGPoint(x: 0, y: 0), CGPoint(x: 2, y: 2), CGPoint(x: 3.2, y: 3.4), CGPoint(x: 4, y: 5)]])
 
     }
 
