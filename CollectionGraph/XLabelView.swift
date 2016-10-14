12872a973bae466c8eaf8fcb62f6e36163fec9b5
be-bert <bebert.apps@gmail.com>
2016-10-14 08:42:40 -0400

    Added XLabelView.swit and XLabelViewAttributes.swift.

---
+++ CollectionGraph/XLabelView.swift
@@ -0,0 +1,41 @@
+//
+//  XLabelView.swift
+//  CollectionGraph
+//
+//  Created by Ben Lambert on 10/14/16.
+//  Copyright © 2016 Collective Idea. All rights reserved.
+//
+
+import UIKit
+
+class XLabelView: UICollectionReusableView {
+    
+    var label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        label.font = UIFont.systemFont(ofSize: 8)
+        label.textAlignment = .center
+        label.textColor = UIColor.white
+        addSubview(label)
+    }
+    
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        
+        if let attributes = layoutAttributes as? XLabelViewAttributes {
+            label.text = attributes.text
+            label.sizeToFit()
+            label.frame = attributes.bounds
+        }
+    }
+
+}
