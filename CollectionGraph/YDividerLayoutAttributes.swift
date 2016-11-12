899ca1cd8774cec85c5c4c91b51196d4254916ee
be-bert <bebert.apps@gmail.com>
2016-11-11 19:30:20 -0500

    Added copyWithZone and isEqual to yDividerLayoutAttributes so updates to their views happen properly.

--- CollectionGraph/YDividerLayoutAttributes.swift
+++ CollectionGraph/YDividerLayoutAttributes.swift
@@ -11,4 +11,23 @@ import UIKit
 class YDividerLayoutAttributes: UICollectionViewLayoutAttributes {
     var text: String = ""
     var inset: CGFloat = 0
+
+    override func copy(with zone: NSZone? = nil) -> Any {
+        let copy = super.copy(with: zone)
+        if let copy = copy as? YDividerLayoutAttributes {
+            copy.text = text
+            copy.inset = inset
+        }
+        return copy
+    }
+
+    override func isEqual(_ object: Any?) -> Bool {
+        if let attributes = object as? YDividerLayoutAttributes {
+            if attributes.text == text && attributes.inset == inset {
+                return super.isEqual(object)
+            }
+        }
+        return false
+    }
+
 }
