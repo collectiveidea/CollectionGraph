7708f35978da227dd3a6b5c6d99babd41b31dd65
be-bert <bebert.apps@gmail.com>
2016-11-14 08:29:08 -0500

    Override copy(with Zone:) and isEqual() to supply the correct behavior when refreshing Supplementary views.

--- CollectionGraph/XLabelViewAttributes.swift
+++ CollectionGraph/XLabelViewAttributes.swift
@@ -10,4 +10,21 @@ import UIKit
 
 class XLabelViewAttributes: UICollectionViewLayoutAttributes {
     var text = ""
+    
+    override func copy(with zone: NSZone? = nil) -> Any {
+        let copy = super.copy(with: zone)
+        if let copy = copy as? XLabelViewAttributes {
+            copy.text = text
+        }
+        return copy
+    }
+    
+    override func isEqual(_ object: Any?) -> Bool {
+        if let attributes = object as? XLabelViewAttributes {
+            if attributes.text == text {
+                return super.isEqual(object)
+            }
+        }
+        return false
+    }
 }
