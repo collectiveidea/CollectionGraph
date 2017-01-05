5cde456f2427b80e1f2b7056b47df13a3aab48e3
be-bert <bebert.apps@gmail.com>
2017-01-05 15:11:50 -0500

    Add RangeFinder.swift to find the max and min x and y values in the graph data.

---
+++ CollectionGraph/RangeFinder.swift
@@ -0,0 +1,54 @@
+//
+//  RangeFinder.swift
+//  CollectionGraph
+//
+//  Created by Ben Lambert on 1/5/17.
+//  Copyright © 2017 Collective Idea. All rights reserved.
+//
+
+import Foundation
+
+protocol RangeFinder {
+    func xDataRange(graphData: [[GraphDatum]]) -> (min: CGFloat, max: CGFloat)
+    func yDataRange(graphData: [[GraphDatum]], numberOfSteps steps: Int) -> (min: CGFloat, max: CGFloat)
+}
+
+extension RangeFinder {
+    func xDataRange(graphData: [[GraphDatum]]) -> (min: CGFloat, max: CGFloat) {
+        let xVals = graphData.flatMap {
+            return $0.map { return $0.point.x }
+        }
+
+        if let min = xVals.min(), let max = xVals.max() {
+            return (min, max - min)
+        }
+
+        return (0, 0)
+    }
+
+    func yDataRange(graphData: [[GraphDatum]], numberOfSteps steps: Int) -> (min: CGFloat, max: CGFloat) {
+        let yVals = graphData.flatMap {
+            return $0.map { return $0.point.y }
+        }
+
+        let maxY = yVals.max() ?? 0
+
+        var remainder = maxY.truncatingRemainder(dividingBy: CGFloat(steps))
+        if remainder.isNaN {
+            remainder = 0
+        }
+
+        var max: CGFloat = 0
+
+        if remainder == 0 {
+            max = maxY
+        } else {
+            max = maxY - remainder + CGFloat(steps)
+        }
+
+        let min = yVals.min() ?? 0
+
+        return (min, max)
+    }
+
+}
