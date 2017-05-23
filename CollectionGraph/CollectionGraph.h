829051a5e3eb1f47c89f05e8662a72e4e563bce4
be-bert <bebert.apps@gmail.com>
2017-05-23 11:55:32 -0400

    Initial Commit

---
+++ CollectionGraph/CollectionGraph.h
@@ -0,0 +1,19 @@
+//
+//  CollectionGraph.h
+//  CollectionGraph
+//
+//  Created by Ben Lambert on 5/23/17.
+//  Copyright © 2017 collectiveidea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CollectionGraph.
+FOUNDATION_EXPORT double CollectionGraphVersionNumber;
+
+//! Project version string for CollectionGraph.
+FOUNDATION_EXPORT const unsigned char CollectionGraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CollectionGraph/PublicHeader.h>
+
+
